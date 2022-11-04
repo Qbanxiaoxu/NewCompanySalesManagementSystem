56b5b93ba14ecdf529ad4d05000d5c54529727fc
qbanxiaoxu <qbanxiaoxu@163.com>
2022-11-04 11:53:56 +0800

    Initial commit

---
+++ src/main/java/com/example/csms/NewCompanySalesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.csms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCompanySalesManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewCompanySalesManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/csms/NewCompanySalesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCompanySalesManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

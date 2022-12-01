9ba1a4cad99d88401021b1d01b0b9c03f5b32dd0
Laura0821 <2829340358@qq.com>
2022-12-01 23:26:34 +0800

    修改界面，增加内容，未完成

--- src/main/resources/static/js/administratorInterface.js
+++ src/main/resources/static/js/administratorInterface.js
@@ -1,121 +1,136 @@
-$(document).ready(() => {
+$(function (){
+    //点击显示员工管理
+    $("#staffManagement").click("click",function () {
+        $("#staffManagementBlock").css("display","block");
+    });
+    $("#showInsertStaffBtn").click("click",function () {
+        $("#insertStaffForm").css("display","block");
+    });
+    $("#showModifyStaffBtn").click("click",function () {
+        $("#modifyStaffForm").css("display","block");
+    })
+    //点击显示用户管理
+    $("#clientManagement").click("click",function () {
+        $("#clientManagementBlock").css("display","block");
+    });
+    $("#showInsertClientBtn").click("click",function(){
+       $("#insertClientForm").css("display","block");
+    });
+    $("#showModifyClientBtn").click("click",function(){
+        $("#modifyClientForm").css("display","block");
+    });
+    //点击显示产品管理
+    $("#companyManagement").click("click",function(){
+        $("#orderManagementBlock").css("display","block");
+    });
+    $("#showInsertProductBtn").click("click",function () {
+        $("#insertProductForm").css("display","block");
+    });
+    $("#showModifyBtn").click("click",function () {
+        $("#modifyProductForm").css("display","block");
+    })
+    $("#getAllBtn").click("click",function () {
+        getAllProducts();
+    });
+    $("#getProductByIdBtn").click("click",function () {
+        let pId=$("#searchBox").val();
+        getProductById(pId);
+    });
+    $("#delProductBtn").click("click",function () {
+        let pId=$("#delBox").val();
+        deleteProductById(pId);
+    })
+    $("#modifyBtn").click("click",function () {
+        modifyProduct();
+    })
+    $("#insertBtn").click("click",function () {
+        insertProduct();
+    })
+    //对销售人员进行操作的函数
+    function getAllStaff() {
+        $.post(
+            "/Query",
+            {object:SalesStaff},
+            function(data){
+                $("#showStaffTable").find('tr').remove();
+                for(i in data){
+                    var tds;
+                    tds='<td>'+data[i].clientId+'</td>'+
+                        '<td>'+data[i].clientPassword+'</td>'+
+                        '<td>'+data[i].clientName+'</td>'+
+                        '<td>'+data[i].clientGender+'</td>'+
+                        '<td>'+data[i].clientAddress+'</td>'+
+                        '<td>'+data[i].clientEmail+'</td>';
+                    $("#showStaffTable").append('<tr>'+tds+'</tr>');
+                }
+            }
+        )
+    }
+    //对产品进行操作的函数
+    function getAllProducts(){
+        $.post(
+            "/Query",
+            {object:"Product"},
+            function (data) {
+                $("#showTable").find('tr').remove();
+                for(i in data){
+                    var tds;
+                    tds='<td>'+data[i].productId+'</td>'+
+                        '<td>'+data[i].productName+'</td>'+
+                        '<td>'+data[i].productDescription+'</td>'+
+                        '<td>'+data[i].productPrice+'</td>'+
+                        '<td>'+data[i].productInventory+'</td>';
+                    $("#showTable").append('<tr>'+tds+'</tr>');
+                }
+            }
+        );
+    }
+    function getStaffById(){
+        $.post(
+            "/Find",
+            {object}
+        )
+    }
+    function getProductById(pId) {
+        pId=$("#searchProductBox").val();
+        $.post(
+            "/Find",
+            {object:"Product",ID:pId},
+            function (data) {
+                if(data!=null){
+                    $("#showTable").find('tr').remove();
+                    let tds;
+                    tds='<td>'+data.productId+'</td>'+
+                        '<td>'+data.productName+'</td>'+
+                        '<td>'+data.productDescription+'</td>'+
+                        '<td>'+data.productPrice+'</td>'+
+                        '<td>'+data.productInventory+'</td>';
+                    $("#showTable").append('<tr>'+tds+'</tr>');
+                }
+            },
+            "json"
+        );
+    }
+    function deleteProductById(pId){
+        $.get(
+            "/Delete",
+            {object:'Product',ID:pId}
+        )
+    }
+    function insertProduct() {
+        let name=$("#insertName").val();
+        let description=$("#insertDescription").val().parseFloat();
+        let price=$("#insertPrice").val();
+        let inventory=$("#insertInventory").val().parseInt();
+        $.post(
 
+        )
+    }
+    function modifyProduct() {
+        let name=$("#modifyName").val();
+        let description=$("#modifyDescription").val().parseFloat();
+        let price=$("#modifyPrice").val();
+        let inventory=$("#modifyInventory").val().parseInt();
+    }
 
-
-    // $("#displayPersonalInfo").click(function(){
-    //     $.post("CompanyBusinessInformationManagement",
-    //         {
-    //             operation:"queryAdministratorInformation",
-    //             identity:"administrator",
-    //             ID:document.getElementById("id").value()
-    //         },
-    //         function(data,status){
-    //             if (status =="success") {
-    //                 $("#display").html("");
-    //                 let personalList = JSON.parse(data);
-    //                 let html = "<table style=\"width: 100%\" border='1px'> <tr><th colspan=\"5\">个人信息</th></tr><tr><td>编号</td><td>姓名</td><td>性别</td><td>邮箱</td><td>住址</td></tr>"
-    //                 for (let i = 0; i < personalList.length; i++) {
-    //                     let personal = personalList[i];
-    //                     html += "<tr>";
-    //                     html += "<td>" + personal.aid + "</td>";
-    //                     html += "<td>" + personal.aname + "</td>";
-    //                     html += "<td>" + personal.agender + "</td>";
-    //                     html += "<td>" + personal.aemail + "</td>";
-    //                     html += "<td>" + personal.aaddress + "</td>";
-    //                     html += "</tr>";
-    //                 }
-    //                 html += "</table>"
-    //                 document.getElementById("display").innerHTML = html;
-    //             } else {
-    //                 alert(data + "\n" + status)
-    //             }
-    //         });
-    // });
-    //
-    // $("#displayProductsInfo").click(function(){
-    //     $.post("ProductInformationManagement",
-    //         {
-    //             operation:"queryProductInformation",
-    //             identity:"administrator"
-    //         },
-    //         function(data,status){
-    //             if(status==="success") {
-    //                 $("#display").html("");
-    //                 let productsList = JSON.parse(data);
-    //                 let html="<table style=\"width: 100%\" border=\"1px\"> <tr><th colspan=\"5\">产品信息</th></tr><tr><td>编号</td><td>名称</td><td>描述</td><td>价格</td><td>库存量</td></tr>"
-    //                 for (let i = 0; i < productsList.length; i++) {
-    //                     let products = productsList[i]
-    //                     html += "<tr>";
-    //                     html += "<td>" + products.pid + "</td>";
-    //                     html += "<td>" + products.pname + "</td>";
-    //                     html += "<td>" + products.pdescription + "</td>";
-    //                     html += "<td>" + products.pprice + "</td>";
-    //                     html += "<td>" + products.pinventory + "</td>";
-    //                     html += "</tr>";
-    //                 }
-    //                 html+="</table>"
-    //                 document.getElementById("display").innerHTML = html;
-    //             }
-    //             else {alert( data + "\n" + status)}
-    //         });
-    // });
-    // $("#findProductInfo").click(() => {
-    //     $("#display").html("");
-    //     document.getElementById("display").innerHTML="产品编号：<input type='number' placeholder='请输入产品编号' id='productID'><input type='button' value='查询产品信息' name='findProductInfo' id='findProductInformation'>";
-    //     $("#findProductInformation").click(function(){
-    //         $.post("ProductInformationManagement",
-    //             {
-    //                 operation:"findProductInformation",
-    //                 identity:"administrator",
-    //                 id:document.getElementById("productID").value()
-    //             },
-    //             function(data,status){
-    //                 if(status==="success") {
-    //                     let productList = JSON.parse(data);
-    //                     let html="<table style=\"width: 100%\" border='1px'> <tr><th colspan=\"5\">产品信息</th></tr><tr><td>编号</td><td>名称</td><td>描述</td><td>价格</td><td>库存量</td></tr>"
-    //                     for (let i = 0; i < productList.length; i++) {
-    //                         let product = productList[i];
-    //                         html += "<tr>";
-    //                         html += "<td>" + product.pid + "</td>";
-    //                         html += "<td>" + product.pname + "</td>";
-    //                         html += "<td>" + product.pdescription + "</td>";
-    //                         html += "<td>" + product.pprice + "</td>";
-    //                         html += "<td>" + product.pinventory + "</td>";
-    //                         html += "</tr>";
-    //                     }
-    //                     html+="</table>"
-    //                     document.getElementById("display").innerHTML = html;
-    //                 }
-    //                 else {alert( data + "\n" + status)}
-    //             });
-    //     });
-    // });
-    //
-    // $("#displayOrdersInfo").click(function(){
-    //     $.post("OrderManagement",
-    //         {
-    //             operation:"queryOrderInformation",
-    //             identity:"administrator",
-    //         },
-    //         function(data,status){
-    //             if(status==="success") {
-    //                 $("#display").html("");
-    //                 let ordersList = JSON.parse(data);
-    //                 let html="<table style=\"width: 100%\" border='1px'> <tr><th colspan=\"5\">订单信息</th></tr><tr><td>订单编号</td><td>下单时间</td><td>客户编号</td><td>销售人员编号</td><td>消费额</td></tr>"
-    //                 for (let i = 0; i < ordersList.length; i++) {
-    //                     let orders = ordersList[i];
-    //                     html += "<tr>";
-    //                     html += "<td>" + orders.oid + "</td>";
-    //                     html += "<td>" + orders.otime + "</td>";
-    //                     html += "<td>" + orders.oclients + "</td>";
-    //                     html += "<td>" + orders.osales + "</td>";
-    //                     html += "<td>" + orders.ototalAmount + "</td>";
-    //                     html += "</tr>";
-    //                 }
-    //                 html+="</table>"
-    //                 document.getElementById("display").innerHTML = html;
-    //             }
-    //             else {alert( data + "\n" + status)}
-    //         });
-    // });
 });
